7ffc4cd00bb27111cfb3959b9619f5f00d35abbb
MohAl23 <alamin_moh@outlook.com>
2024-02-29 15:48:18 +0000

    colour changes

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-const ul = document.querySelector('ul');
-const input = document.getElementById('item');
-const addBtn = document.getElementById('addBtn');
-const deleteBtn = document.getElementById('deleteBtn');
-let itemsArray = JSON.parse(localStorage.getItem('items')) || [];
+const ul = document.querySelector("ul");
+const input = document.getElementById("item");
+const addBtn = document.getElementById("addBtn");
+const deleteBtn = document.getElementById("deleteBtn");
+let itemsArray = JSON.parse(localStorage.getItem("items")) || [];
 
 function inputLength() {
-    return input.value.length;
+  return input.value.length;
 }
 
 function addTask() {
-    if (inputLength() > 0) {
-        itemsArray.push(input.value);
-        localStorage.setItem('items', JSON.stringify(itemsArray));
-        addTaskToList(input.value);
-        input.value = '';
-    } else {
-        alert('Please enter a task.');
-    }
+  if (inputLength() > 0) {
+    itemsArray.push(input.value);
+    localStorage.setItem("items", JSON.stringify(itemsArray));
+    addTaskToList(input.value);
+    input.value = "";
+  } else {
+    alert("Please enter a task.");
+  }
 }
 
 function deleteTasks() {
-    localStorage.clear();
-    ul.innerHTML = '';
-    itemsArray = [];
+  localStorage.clear();
+  ul.innerHTML = "";
+  itemsArray = [];
 }
 
 function addTaskToList(text) {
-    const li = document.createElement('li');
-    li.textContent = text;
-    ul.appendChild(li);
+  const li = document.createElement("li");
+  li.textContent = text;
+  ul.appendChild(li);
 }
 
-addBtn.addEventListener('click', addTask);
-deleteBtn.addEventListener('click', deleteTasks);
+addBtn.addEventListener("click", addTask);
+deleteBtn.addEventListener("click", deleteTasks);
 
 itemsArray.forEach(addTaskToList);
